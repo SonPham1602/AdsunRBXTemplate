d733c09dfcd427f1ca2e97f02cba46d04044aeb4
SonPham1602 <43876888+SonPham1602@users.noreply.github.com>
2020-05-26 15:09:18 +0700

    update

--- js/caller.js
+++ js/caller.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
-    setTimeout(()=>{
-        $("#callerPhone").fadeIn(100)
-    },5000)
+    // setTimeout(()=>{
+    //     $("#callerPhone").fadeIn(100)
+    // },5000)
+    $("#cancelAcceptCaller").on('click',function()
+    {
+        $("#acceptCallerPhone").fadeOut(200)
+    })
+    $(".aspectCall").on('click',function(e)
+    {
+        $("#callerPhone").hide()
+        $("#acceptCallerPhone").show()
+        var dem = 0
+        var giay,phut
+        setInterval(()=>{
+            dem++
+            if(dem<60)
+            {
+                giay=dem
+                $("#timeCaller").text("00:"+dem)
+            }
+            else{
+                giay = dem%60
+                phut = Math.floor(dem/60).
+                $("#timeCaller").text( phut+ ":"+giay)
+            }
+
+        },1000)
+    }
+    )
 })
